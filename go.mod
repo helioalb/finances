16bc79b06d7249f381bc2c6ec0e233285ae10c9a
Helio Albano de Oliveira <helioalb@gmail.com>
2025-12-28 14:50:48 -0300

    Add postgres connector

--- go.mod
+++ go.mod
@@ -3,3 +3,8 @@ module github.com/helioalb/finances
 go 1.25.3
 
 require github.com/google/uuid v1.6.0
+
+require (
+	github.com/DATA-DOG/go-sqlmock v1.5.2
+	github.com/lib/pq v1.10.9 // indirect
+)
---
+++ pkg/postgres/pg.go
@@ -0,0 +1,40 @@
+package postgres
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/lib/pq"
+)
+
+type Config struct {
+	Host     string
+	Port     string
+	User     string
+	Password string
+	DBName   string
+	SSLMode  string
+}
+
+func Connect(cfg Config) (*sql.DB, error) {
+	if cfg.SSLMode == "" {
+		cfg.SSLMode = "disable"
+	}
+
+	connStr := fmt.Sprintf(
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
+		cfg.Host, cfg.Port, cfg.User, cfg.Password, cfg.DBName, cfg.SSLMode,
+	)
+
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		return nil, fmt.Errorf("failed to open database connection: %w", err)
+	}
+
+	if err := db.Ping(); err != nil {
+		db.Close()
+		return nil, fmt.Errorf("failed to ping database: %w", err)
+	}
+
+	return db, nil
+}
