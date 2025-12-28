bb8b79f6ba73e580c29e519e9fe7e43c751022d9
Helio Albano de Oliveira <helioalb@gmail.com>
2025-12-27 21:06:55 -0300

    Add user entity

---
+++ internal/user/entity.go
@@ -0,0 +1,15 @@
+package user
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type User struct {
+	ID        int64
+	UUID      uuid.UUID
+	Name      string
+	CreatedAt time.Time
+	UpdatedAt time.Time
+}
