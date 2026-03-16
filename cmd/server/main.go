72be2e1e8291e147d1e9a4fb713bb25d93c222d6
Helio Albano de Oliveira <helioalb@gmail.com>
2026-03-16 08:46:12 -0300

    Add shutdown graceful

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,10 +1,16 @@
 package main
 
 import (
+	"context"
 	"database/sql"
+	"errors"
 	"log"
 	"log/slog"
+	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/helioalb/finances/configs"
 	"github.com/helioalb/finances/internal/user"
@@ -13,18 +19,40 @@ import (
 )
 
 func main() {
+	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer stop()
+
 	e := setupEcho()
+
 	db := setupDatabase()
+	defer db.Close()
+
 	l := setupLogger()
 
 	user.Init(e, db, l)
 
 	l.Info("[server_starting][address=:8080]")
-	e.Start(":8080")
+	go func() {
+		if err := e.Start(":8080"); err != nil && !errors.Is(err, http.ErrServerClosed) {
+			l.Error("[server_error][message=failed_to_start_server]", "error", err)
+			stop()
+		}
+	}()
+
+	<-ctx.Done()
+	l.Info("[server_shutting_down]")
+
+	shutdownCtx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	if err := e.Shutdown(shutdownCtx); err != nil {
+		l.Error("[server_error][message=failed_to_shutdown_server]", "error", err)
+	}
 }
 
 func setupEcho() *echo.Echo {
 	e := echo.New()
+	e.HideBanner = true
 
 	return e
 }
